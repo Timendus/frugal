70f20ccc53e7f9c6d8e6e2fa1ffaa3db6e63bb4b
Timendus <mail@timendus.com>
2025-03-19 17:30:11 +0100

    Allow search to find domains and links

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -17,26 +19,83 @@ type SearchResult struct {
 	Snippet string `json:"snippet"`
 }
 
+type Link struct {
+	URL   string `json:"url"`
+	Title string `json:"title"`
+}
+
 const SITES_DIR = "./config/root"
-const CONTEXT = 100
+const LINKS_FILE = "./config/links.json"
+const SITES_FILE = "./config/websites.txt"
+const SEARCH_CONTEXT = 100
+
+var domains []Link
 
 func main() {
+	// Which port should we host on?
 	port := os.Getenv("SERVER_PORT")
 	if port == "" {
 		port = "3000"
 	}
+
+	err := preloadSearchableDomains()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Start the server
 	fmt.Println("Starting HTTP server on http://localhost:" + port)
 	http.HandleFunc("/search", searchHandler)
 	http.HandleFunc("/links.json", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./config/links.json")
+		http.ServeFile(w, r, LINKS_FILE)
 	})
 	http.Handle("/", http.FileServer(http.Dir(SITES_DIR)))
 	server := &http.Server{Addr: ":" + port}
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	fmt.Println(err)
 }
 
-// searchHandler reads the "q" query parameter and searches through all files under ./sites.
+func preloadSearchableDomains() error {
+	// Which domains do we have in the links section?
+	configFile, err := os.Open(LINKS_FILE)
+	if err != nil {
+		return err
+	}
+	configBytes, _ := io.ReadAll(configFile)
+	var links []Link
+	err = json.Unmarshal(configBytes, &links)
+	if err != nil {
+		return err
+	}
+	for _, link := range links {
+		domains = append(domains, link)
+	}
+
+	// Which domains do we have cached?
+	file, err := os.Open(SITES_FILE)
+	if err != nil {
+		return err
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.Split(strings.ToLower(scanner.Text()), "://")
+		if len(parts) != 2 {
+			// That's a weird URL. Ignore it.
+			continue
+		}
+		domains = append(domains, Link{
+			URL:   "/" + parts[1],
+			Title: parts[1],
+		})
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the query term.
 	query := strings.ToLower(r.URL.Query().Get("q"))
@@ -45,8 +104,28 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Walk through the directory recursively.
 	var results []SearchResult
+
+	// Find matches with domains
+	for _, domain := range domains {
+		if idx := strings.Index(strings.ToLower(domain.Title), query); idx >= 0 {
+			results = append(results, SearchResult{
+				Title:   domain.Title,
+				Path:    domain.URL,
+				Snippet: "",
+			})
+			continue
+		}
+		if idx := strings.Index(strings.ToLower(domain.URL), query); idx >= 0 {
+			results = append(results, SearchResult{
+				Title:   domain.Title,
+				Path:    domain.URL,
+				Snippet: "",
+			})
+		}
+	}
+
+	// Walk through the directory recursively.
 	err := filepath.Walk(SITES_DIR, func(path string, info os.FileInfo, err error) error {
 		if len(results) > 100 {
 			// We've seen enough hits, stop searching
@@ -82,11 +161,11 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		text := strings.ToLower(extractText(doc))
 		if idx := strings.Index(text, query); idx >= 0 {
 			// Build a snippet from the text content (not including markup).
-			start := idx - CONTEXT
+			start := idx - SEARCH_CONTEXT
 			if start < 0 {
 				start = 0
 			}
-			end := idx + CONTEXT + len(query)
+			end := idx + SEARCH_CONTEXT + len(query)
 			if end > len(text) {
 				end = len(text)
 			}
